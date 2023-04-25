6045d51c45a99d64fc6dd45ea586ed4a6db6766b
akii <111143540+akii56@users.noreply.github.com>
2023-04-25 21:37:21 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -32,16 +32,14 @@ let kalkulator = {
         let resenje = prvi_broj / drugi_broj
         console.log(`${prvi_broj} / ${drugi_broj} = ${resenje}`)
     },
-    koren: function(prvi_broj, drugi_broj) {
-        let resenje = Math.sqrt(prvi_broj);
-        let resenje1 = Math.sqrt(drugi_broj);
-        console.log(`Koren broja ${prvi_broj} je ${resenje}, a koren broja ${drugi_broj} je ${resenje1}`);
-        },
-        na_kvadrat: function() {
-        let broj = Math.pow(9, 2);
-        let broj1 = Math.pow(4,2);
-        console.log(`9 na kvadrat je ${broj}, a 4 na kvadrat je ${broj1}`); 
-}
+    koren: function(x) {
+        let korenovan = Math.sqrt(x)
+        console.log(`Koren broja ${x} je ${korenovan}`);
+    },
+    na_kvadrat: function(x) {
+        let kvadriran = Math.pow(x,2)
+        console.log(`Kvadrat broja ${x} je ${kvadriran}`); 
+    }
 }
 
 
@@ -51,9 +49,5 @@ kalkulator.sabiranje(9,4);
 kalkulator.oduzimanje(9,4);
 kalkulator.mnozenje(9,4);
 kalkulator.deljenje(9,4);
-kalkulator.koren(9,4);
-kalkulator.na_kvadrat();
-
-
-
-
+kalkulator.koren(4);
+kalkulator.na_kvadrat(4);
